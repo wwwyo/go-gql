83086646add9c032d07b8a04c5a57e93ccf0fa46
yuito.watanabe <w.ytmrm11811@gmail.com>
2023-05-14 15:31:31 +0900

    chore: gqlgen

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,23 +8,24 @@ import (
 	"context"
 	"fmt"
 	"my_gql_server/graph/model"
+	"my_gql_server/internal"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-
+	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
 }
 
 // Todos is the resolver for the todos field.
-func (resolver *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented: Todos - todos"))
 }
 
-// Mutation returns MutationResolver implementation.
-func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+// Mutation returns internal.MutationResolver implementation.
+func (r *Resolver) Mutation() internal.MutationResolver { return &mutationResolver{r} }
 
-// Query returns QueryResolver implementation.
-func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+// Query returns internal.QueryResolver implementation.
+func (r *Resolver) Query() internal.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
